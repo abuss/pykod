b66664bd4df221aef4eaeb11c09d047cab361263
Antal Buss <antal.buss@gmail.com>
2025-12-29 01:02:28 -0700

    Updated mirror

--- example/configuration-vm.py
+++ example/configuration-vm.py
@@ -46,7 +46,7 @@ from pykod.user import (
 
 # from pykod.repositories import Repository
 
-archpkgs = Arch(mirror_url="https://mirror.rackspace.com/archlinux")
+archpkgs = Arch(mirror_url="ttps://mirror.cpsc.ucalgary.ca/mirror/archlinux.org/")
 # aurpkgs = AUR(helper="yay", helper_url="https://aur.archlinux.org/yay-bin.git")
 aurpkgs = AUR(helper="paru", helper_url="https://aur.archlinux.org/paru.git")
 flatpakpkgs = Flatpak(hub_url="flathub")
